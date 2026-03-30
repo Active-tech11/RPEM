cc335bd70e1b958a6f93ef6bc59cf8ad70e31c17
Active-tech11 <shanshuiqiankun@gmail.com>
2026-03-30 17:45:50 +0800

    Update EntropyCal.py

--- src/EntropyCal.py
+++ src/EntropyCal.py
@@ -92,45 +92,4 @@ if __name__ == '__main__':
 
     print(f"ALL ENTROPY: {All_Entropy}")
 
-"""
-def calculate_signal_strength_per_path(adj_list , path) :
-    signal = 1.0  # 从源节点出发，信号量为1
-    for i , node in enumerate ( path[1 :-1] , 1 ) :  # 排除目标节点
-        degree = len ( adj_list[node] )
-        signal /= degree  # 根据节点度数分裂信号
-    return signal
 
-def calculate_entropy(adj_list , paths , source_node , target_node) :
-    path_signals = calculate_signal_strength ( adj_list , paths , source_node , target_node )
-
-    # 计算每条路径的信息熵
-    entropies = [-signal * math.log ( signal , 2 ) for signal in path_signals if signal > 0]
-
-    # 计算总信息熵
-    total_entropy = sum ( entropies )
-    return total_entropy"""
-
-
-"""def calculate_signal(adj_list, paths):
-    # 初始化所有节点的信号量为0
-    node_signal = {node: 0 for node in adj_list}
-    node_signal[source_node] = 1  # 源节点信号量设置为1
-
-    # 按路径长度从小到大排序
-    paths.sort(key=len)
-
-    # 逐条路径计算信号量
-    for path in paths:
-        signal = node_signal[path[0]]  # 获取路径起始节点的信号量
-        for node in path[1:]:  # 从路径的第二个节点开始计算
-            signal /= len(adj_list[node])  # 按节点度数衰减信号量
-            node_signal[node] += signal  # 累加信号量到当前节点
-
-    # 最终目标节点的信号量即为所有路径上信号量的总和
-    final_signal = node_signal[target_node]
-    return final_signal
-
-
-# 计算信号量总和
-total_signal = calculate_signal(adj_list, paths)
-print(f"Total signal strength at node {target_node} is: {total_signal}")"""
